7166fac0631b305403ef315a00ab43ba2773914a
KimGyeongBln <hhk3950@naver.com>
2024-11-07 13:19:21 +0900

    다형성 (오버로딩)

--- src/com/javaex/practice02/Ex06.java
+++ src/com/javaex/practice02/Ex06.java
@@ -1,30 +1,11 @@
 package com.javaex.practice02;
 
-import java.util.Scanner;
-
 public class Ex06 {
 
 	public static void main(String[] args) {
+		System.out.println("나이를 입력하세요");
+		System.out.print("나이: ");
 		
-	
-	int age;
-	
-	
-	Scanner sc = new Scanner(System.in);
-	
-	System.out.println("나이를 입력해주세요");
-	System.out.print("나이: ");
-	age = sc.nextInt();
-	 if(age > 20) {
-		 System.out.print("\"1번그룹\"");
-	}else { if(age < 20);
-	System.out.println("\"2번그룹\"");
-		}
-	
-	 System.out.println("입니다");
-	sc.close();
- 
-	}	
-}
-  
+	}
 
+}
